ed51cdb5365d5ccb2d632d5e32977edcd3995ee7
Regner Blok-Andersen <shadowdf@gmail.com>
2015-04-10 08:52:08 +0000

    Adding setup file and CLI

---
+++ setup.py
@@ -0,0 +1,31 @@
+
+
+import os
+
+from pip.req    import parse_requirements
+from setuptools import setup, find_packages
+
+from thanatos import __version__
+
+
+try:
+    readme_path     = os.path.join(os.path.dirname(__file__), "README.md")
+    readme_contents = open(readme_path).read()
+
+except IOError:
+    readme_contents = ""
+
+install_reqs = parse_requirements('requirements.txt')
+reqs         = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name="Thanatos",
+    version=__version__,
+    description="A Python library for generating EVE trivia questions.",
+    long_description=readme_contents,
+    license="MIT License",
+    packages=find_packages(),
+    data_files=[('', ['README.md', 'LICENSE'])],
+    install_requires=reqs,
+    scripts=['bin/thanatos'],
+)
