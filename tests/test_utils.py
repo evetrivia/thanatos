109239652f51a369a94172795ea278bce674204f
Regner Blok-Andersen <shadowdf@gmail.com>
2015-04-23 22:17:12 +0000

    Adding test coverave for thanatos.utils.download_tables

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,3 +10,16 @@ class UtilsTestCase(unittest2.TestCase):
 
     def setUp(self):
         pass
+
+    @mock.patch('urllib.urlretrieve')
+    def test_download_tables(self, mock_urlretrieve):
+        base_url    = 'http://example.com/'
+        tables_list = [
+            'table_one',
+            'table_two',
+        ]
+
+        utils.download_tables(tables_list, base_url)
+
+        mock_urlretrieve.assert_any_call('http://example.com/table_one.sql.bz2', 'table_one.sql.bz2')
+        mock_urlretrieve.assert_any_call('http://example.com/table_two.sql.bz2', 'table_two.sql.bz2')
