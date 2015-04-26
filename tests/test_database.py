a76bbe63d6ac9db506c974ad33ccbe8e1eccb368
Regner Blok-Andersen <shadowdf@gmail.com>
2015-04-26 22:19:32 +0000

    Moved tests that I missed in the previous commit

--- tests/test_database.py
+++ tests/test_database.py
@@ -83,3 +83,22 @@ class DatabaseTestCase(unittest2.TestCase):
         database.get_dogma_attribute_for_type(mock_db_connection, mock_type_id, mock_dogma_attribute)
 
         mock_db_connection.cursor().execute.assert_was_called()
+    
+    @mock.patch('urllib.urlretrieve')
+    def test_download_tables(self, mock_urlretrieve):
+        base_url    = 'http://example.com/'
+        tables_list = [
+            'table_one',
+            'table_two',
+        ]
+
+        database.download_tables(tables_list, base_url)
+
+        mock_urlretrieve.assert_any_call('http://example.com/table_one.sql.bz2', 'table_one.sql.bz2')
+        mock_urlretrieve.assert_any_call('http://example.com/table_two.sql.bz2', 'table_two.sql.bz2')
+
+    def test_get_table_filename(self):
+        table_name = 'test_one'
+        table_file_name = database.get_table_filename(table_name)
+
+        self.assertEqual(table_file_name, 'test_one.sql.bz2')
