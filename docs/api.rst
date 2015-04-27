e0ea22840844d3a2ecec3ac93cbf43169ef88c72
Regner Blok-Andersen <shadowdf@gmail.com>
2015-04-27 15:36:07 +0000

    Adding the first bits of the API documentation.

---
+++ docs/api.rst
@@ -0,0 +1,20 @@
+.. _api:
+
+API
+===
+
+.. module:: thanatos
+
+Bordering Regions Question
+--------------------------
+
+.. autoclass:: thanatos.questions.universe.BorderingRegionsQuestion
+   :members:
+   :inherited-members:
+
+Poitot Question
+--------------------------
+
+.. autoclass:: thanatos.questions.universe.PoitotFamousForQuestion
+   :members:
+   :inherited-members:
--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
@@ -89,7 +89,7 @@ exclude_patterns = ['_build']
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
-#add_module_names = True
+add_module_names = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
